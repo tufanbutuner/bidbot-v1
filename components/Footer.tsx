c0bad9b251f7b806ed04f9324824a6062ed197b8
Tufan.Butuner <Tufan.Butuner@transformuk.com>
2023-11-05 12:01:27 +0000

    Navbar and Footer

---
+++ components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <div className="footer-container">
+      <p>Transform &copy; 2023</p>
+    </div>
+  );
+}
---
+++ components/Navbar.tsx
@@ -0,0 +1,11 @@
+export default function Navbar() {
+  return (
+    <div className="navbar-container">
+      <h1>BidBot</h1>
+      <ul>
+        <li>Home</li>
+        <li>Sign in</li>
+      </ul>
+    </div>
+  );
+}
