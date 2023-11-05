0df2d13b4bbbb4640669224ac4dfed8103a0adac
Tufan.Butuner <Tufan.Butuner@transformuk.com>
2023-11-05 20:26:14 +0000

    auth, and return matches and documents

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,22 @@
+import { Session } from "next-auth";
+import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import "../styles/globals.scss";
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({
+  Component,
+  pageProps,
+}: AppProps<{
+  session: Session;
+}>) {
   return (
-    <>
+    <SessionProvider session={pageProps.session}>
       <Navbar />
       <Component {...pageProps} />
       <Footer />
-    </>
+    </SessionProvider>
   );
 }
 
